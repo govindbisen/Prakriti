df33a2d06a8d6d59c9e066c870eb07dec897b1e1
govindbisen <govind.bisen@gmail.com>
2023-07-28 21:18:30 +0530

    something

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+import "./App.css";
+import logo from "./assets/iskon.png";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div>
+          {/* <img src={logo} className="App-logo" alt="ISKON LOGO" /> */}
+          Prakriti
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="#"> About</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="#"> Home </a>
+            </li>
+            <li>
+              <a href="#"> Blog</a>
+            </li>
+            <li>
+              <a href="#"> Zoom Classes</a>
+            </li>
+            <li>
+              <a href="#"> Test</a>
+            </li>
+            <li>
+              <a href="#"> Contacts</a>
+            </li>
+            <li>
+              <a href="#"> Donate</a>
+            </li>
+            <li>
+              <a href="#"> Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <main>
+        <article>article</article>
+        <aside>aside</aside>
+      </main>
+      <footer>
+        <p>©Copyright 2050 by nobody. All rights reversed.</p>
+        <a href="https://youtube.com/@PrakritiMPPSC">YouTube</a> <br></br>
+        <a href="https://www.facebook.com/Prakriti-MPPSC-107311648723955/">
+          FaceBook
+        </a>
+        <br></br>
+        <a href="https://t.me/PrakritiMppsc">Telegram</a>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Zoomclass.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Zoomclass() {
+  return <div>Zoomclass</div>;
+}
+
+export default Zoomclass;
