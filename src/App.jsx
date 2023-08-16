b0e4632e2c483881041d863b651815903faa9451
govind <govind.bisen@gmail.com>
2023-08-16 15:27:17 +0530

    changes in navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import "./App.css";
-import logo from "./assets/prakriti.jpeg";
+
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Blog from "./Pages/Blog";
@@ -11,61 +11,29 @@ import Test from "./Pages/Test";
 import LoginSignup from "./Pages/LoginSignup";
 import Donate from "./Pages/Donate";
 
-import { Link, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+
 
 function App() {
+  const routes = [
+    { path: '/about', element: <About /> },
+    { path: '/', element: <Home /> },
+    { path: '/blog', element: <Blog /> },
+    { path: '/zoomclasses', element: <Zoomclass /> },
+    { path: '/test', element: <Test /> },
+    { path: '/contact', element: <Contact /> },
+    { path: '/donate', element: <Donate /> },
+    { path: '/authentication', element: <LoginSignup /> },
+  ];
+
   return (
     <>
-      <nav>
-        <div className="logo-head">
-          <img src={logo} className="logo" alt="LOGO" />
-          <p> Prakriti MPPSC </p>
-        </div>
-        <div>
-          <ul>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/zoomclasses">Zoom Classes</Link>
-            </li>
-            <li>
-              <Link to="/test">Test</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/donate">Donate</Link>
-            </li>
-            <li>
-              <Link to="/authentication">Login/SignUP</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <main>
-        <article></article>
-        <aside></aside>
-      </main>
-
+      <Navbar/>
       <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/zoomclasses" element={<Zoomclass />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/donate" element={<Donate />} />
-
-        <Route path="/authentication" element={<LoginSignup />} />
+      {routes.map((route) => (
+        <Route key={route.path} path={route.path} element={route.element} />
+      ))}
       </Routes>
     </>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+// import logo from "./assets/prakriti.jpeg";
+import logo from "../assets/prakriti.jpeg"
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    const navigationLinks = [
+        { to: '/about', text: 'About' },
+        { to: '/', text: 'Home' },
+        { to: '/blog', text: 'Blog' },
+        { to: '/zoomclasses', text: 'Zoom Classes' },
+        { to: '/test', text: 'Test' },
+        { to: '/contact', text: 'Contact' },
+        { to: '/donate', text: 'Donate' },
+        { to: '/authentication', text: 'Login/SignUP' },
+      ];
+  return (
+    <nav>
+    <div className="logo-head">
+      <img src={logo} className="logo" alt="LOGO" />
+      <p> Prakriti MPPSC </p>
+    </div>
+    <div>
+    <ul>
+      {navigationLinks.map((link) => (
+        <li key={link.to}>
+          <Link to={link.to}>{link.text}</Link>
+        </li>
+      ))}
+    </ul>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar
